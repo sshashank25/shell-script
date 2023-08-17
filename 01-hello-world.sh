c6712b143d6422df31467e140bab8cc104893912
Shashank <shashank25.abap@gmail.com>
2023-08-16 23:46:21 -0400

    change to the hello world

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "Hello world!. I am learning shell script"
 echo "I am learning Devops with AWS "
+
+PERSON1=Sachin
+PERSON2=Rohit
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
