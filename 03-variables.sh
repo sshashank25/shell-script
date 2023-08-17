a179888ea5d66f5db3661e96919495600154020d
Shashank <shashank25.abap@gmail.com>
2023-08-17 00:08:14 -0400

    syntax errors are corrected to 03-variables.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -9,5 +9,5 @@ echo "$PERSON1: Hi $PERSON2, Good Morning"
 echo "$PERSON2: Good Morning $PERSON1"
 echo "$PERSON1: How are you $PERSON2"
 echo "$PERSON2: I am good, how are you $PERSON1"
-echo "$PERSON1: I am good too. May I know the date today?
+echo "$PERSON1: I am good too. May I know the date today?"
 echo "$PERSON1: Sure, it is $DATE"
