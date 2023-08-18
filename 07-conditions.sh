7ca84ac01e5982469c7bcc9600ff518fb4c19094
Shashank <shashank25.abap@gmail.com>
2023-08-17 21:27:19 -0400

    New file is created: 07-conditions.sh

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -3,9 +3,13 @@
 # check if the number is greater than 10 or not
 NUM=$1
 
+USER=$(id -u)
+
 if [ $NUM -gt 10 ]
 then
     echo "$NUM is greater than 10"
 else
     echo "$NUM is not greater than 10"
 fi
+
+echo "the value is: $USER"
